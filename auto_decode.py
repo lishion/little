f9dca0e01a9d807dc83566e2886ea0b3ee479885
lishion <544670411@QQ.com>
2018-05-21 16:36:46 +0800

    return encoding and add debug output

--- auto_decode.py
+++ auto_decode.py
@@ -1,5 +1,7 @@
 import requests
 
+debug = False
+
 def get_encoding(response):
     encoding_from_meta = requests.utils.get_encodings_from_content(response.text)
     if encoding_from_meta:
@@ -12,12 +14,12 @@ def use_all_encoding(response):
     for encoding in encodings:
         try:
             text =  response.content.decode(encoding)
-            #print(f'[*] using encoding {encoding} success')
-            return text
+            debug and print(f'[*] using encoding {encoding} success')
+            return text,encoding
         except:
-            #print(f'[x] using encoding {encoding} error')
+            debug and print(f'[x] using encoding {encoding} error')
             pass
-    return response.text
+    return response.text,'not find'
 
 def decode(response):
 
@@ -25,16 +27,16 @@ def decode(response):
        
         try:
             encoding = get_encoding(response)
-            #print(f'[*] no charset in http header find ')
+            debug and print(f'[*] no charset in http header find ')
             text =  response.content.decode(encoding)
-            #print(f'[*] using encoding {encoding} success')
-            return text
+            debug and print(f'[*] using encoding {encoding} success')
+            return text,encoding
         except:
-            #print(f'[x] using encoding {encoding} error')
+            debug and print(f'[x] using encoding {encoding} error')
             pass
     else:
 
-        #print(f'[*] ${response.encoding} find in http header ')
-        return response.text
+        debug and print(f'[*] {response.encoding} find in http header ')
+        return response.text,response.encoding
 
     return use_all_encoding(response)
