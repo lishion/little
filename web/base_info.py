a28c6035c052fa99c069d8802c5d8bf3e1c32229
lishion <544670411@QQ.com>
2018-05-22 16:28:04 +0800

    add more functions

--- web/base_info.py
+++ web/base_info.py
@@ -3,7 +3,7 @@ import requests
 import re
 from bs4 import BeautifulSoup
 from auto_decode import decode
-
+from urllib.parse import urlparse
 
 def simple_dns(url):
     """
@@ -40,3 +40,44 @@ def get_title(url):
             return {"title":"","other":"网页中不存在title"}
     except BaseException as e:
         return {"title":"","other":"连接到服务器失败: " + str(e)}
+
+def domain(url):
+    """
+        从url中获取域名:
+        https://www.test.com?q=12313 -> www.test.com
+    """
+    parse_url = url
+    if "://" not in url:
+        parse_url = "http://" + url
+    return urlparse(parse_url).netloc
+
+topHostPostfix = (
+    '.com','cn','la','.io','.co','.info','.net','.org','.me','.mobi',
+    '.us','.biz','.xxx','.ca','.co.jp','.com.cn','.net.cn',
+    '.org.cn','.mx','.tv','.ws','.ag','.com.ag','.net.ag',
+    '.org.ag','.am','.asia','.at','.be','.com.br','.net.br',
+    '.bz','.com.bz','.net.bz','.cc','.com.co','.net.co',
+    '.nom.co','.de','.es','.com.es','.nom.es','.org.es',
+    '.eu','.fm','.fr','.gs','.in','.co.in','.firm.in','.gen.in',
+    '.ind.in','.net.in','.org.in','.it','.jobs','.jp','.ms',
+    '.com.mx','.nl','.nu','.co.nz','.net.nz','.org.nz',
+    '.se','.tc','.tk','.tw','.com.tw','.idv.tw','.org.tw',
+    '.hk','.co.uk','.me.uk','.org.uk','.vg', ".com.hk",".cn",".edu",'.info','.app',
+    '.edu.cn','.gov.cn'
+    
+    )
+regx = r'[^\.]+('+'|'.join([h.replace('.',r'\.') for h in topHostPostfix])+')$'
+pattern = re.compile(regx,re.IGNORECASE)
+
+def basedomian(url):
+    """
+        从url、域名中获取主域名
+        (https://)www.test.com -> test.com
+    """
+    url_trimed = url.strip(" ")
+    if not ( url_trimed.startswith("http://") or url_trimed.startswith("https://") ):
+        url_trimed = "http://" + url_trimed
+    host = urlparse(url_trimed).netloc
+    m = pattern.search(host)
+    res =  m.group() if m else host
+    return res if res else url
